6a9b79d655a7951d8c379098495c31c21ec2e6df
Perro2127f <133118680+Perro2127f@users.noreply.github.com>
2024-01-25 18:15:48 -0500

    Update adyen.php

--- adyen.php
+++ adyen.php
@@ -3,11 +3,12 @@
 header('content-type: application/json');
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
-$lista = $update['lista'];
+$lista = $_GET['lista'];
 if(empty($lista)){
-    echo '{"success":false, "Message":"Please Enter Valid CC"}';
+    echo 'Message":"?lista=cc|mes|ano|cvv&key=';
     exit();
 }
+#$lista = '4728810541126442|12|2029|143';
 $lista2 = $lista;
 preg_match_all('/(\d{15,16})+?[^0-9]+?(\d{1,2})[\D]*?(\d{2,4})[^0-9]+?(\d{3,4})/', $lista, $lista);
 $cc = $lista[1][0];
@@ -18,21 +19,22 @@ $ano = $lista[3][0];
     $ano = "20".$ano;
  }
 $cvv = $lista[4][0];
-$key = $update['key'];
+$key = $_GET['key'];
+
 if(empty($cc) || empty($mes) || empty($ano) || empty($cvv)){
-    echo '{"success":false, "Message":"Please Enter Valid CC"}';
+    echo 'Message":"?lista=cc|mes|ano|cvv&key=';
     exit();
 }
 if(empty($key)){
-    echo '{"success":false, "Message":"Please Enter The Adyen Key"}';
+    echo 'Message":"Adyen Key empty"';
     exit();
 
 }
 $lista = "$cc|$mes|$ano|$cvv";
-$encrypt = shell_exec("CC=$cc MES=$mes ANO=$ano CVV=$cvv NAME='Juan Perez' KEY='$key' node encrypt/index.js");
+$encrypt = shell_exec("CC=$cc MES=$mes ANO=$ano CVV=$cvv NAME='Jhin Vega' KEY='$key' node encrypt/index.js");
 
 if(empty($encrypt)){
-echo '{"success":false, "Message":"Encrypt Failed"}';
+echo 'Encrypt Failed';
 }else{
     echo $encrypt;
 }
